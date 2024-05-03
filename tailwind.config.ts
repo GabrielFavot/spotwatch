0b622a41b6b1302e8cac5877f6649deca80e0049
Gabriel Favot <Gabriel.favotwurtz@gmail.com>
2024-05-03 21:10:58 +0200

    feat: add missing tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,19 @@
 import type { Config } from 'tailwindcss';
 
-export default <Partial<Config>>{};
+export default <Partial<Config>>{
+  theme: {
+    extend: {
+      colors: {
+        spotify: {
+          green: '#1db954',
+          dark: '#212121',
+          darker: '#121212',
+          gray: '#535353',
+          lightgray: '#b3b3b3',
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
