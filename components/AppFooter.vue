63c62a7b4f2a3645df159b53edb22834eac1fbe8
Gabriel Favot <Gabriel.favotwurtz@gmail.com>
2024-05-04 17:11:59 +0200

    feat: move contributors into own component

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,22 +1,9 @@
 <template>
-  <footer class="bg-spotify-dark h-8 w-full z-20 fixed bottom-0">
-    <div class="flex flex-row justify-around w-full">
-      <span class="text-spotify-lightgray"><Icon name="ic:baseline-code" class="text-spotify-green" /> with <Icon class="text-red-400 animate-pulse" name="material-symbols:favorite-outline" /> and  by <a :href="randomContributor?.url" class="hover:underline capitalize">{{ randomContributor?.login }}</a> </span>
-    </div>
+  <footer class="bg-spotify-dark h-8 w-full z-20 fixed bottom-0 flex items-center justify-center">
+    <Contributors :contributors="contributors" />
   </footer>
 </template>
 
 <script lang="ts" setup>
 const { data: contributors } = await useFetch<{ login: string; html_url: string }[]>('https://api.github.com/repos/GabrielFavot/spotilive/contributors');
-
-const randomContributor = useState(() => {
-  if (!contributors.value) return null;
-
-  const randomContributor = contributors.value[Math.floor(Math.random() * contributors.value.length)];
-
-  return {
-    login: randomContributor.login,
-    url: randomContributor.html_url,
-  };
-});
 </script>
---
+++ components/Contributors.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-row items-center text-spotify-lightgray font-thin gap-2">
+    <Icon name="ic:baseline-code" class="text-spotify-green" />
+    <span>with</span>
+    <Icon class="text-red-400 animate-pulse" name="material-symbols:favorite-outline" />
+    <span>and</span>
+    <Icon class="text-yellow-400" name="ph:beer-stein" />
+    <span>by</span>
+    <a :href="randomContributor?.url" class="hover:underline font-normal">{{ randomContributor?.login }}</a>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const { contributors } = defineProps(
+  {
+    contributors: Array as PropType<{ login: string; url: string }[]>,
+  },
+);
+
+const randomContributor = computed(() => {
+  if (!contributors) return null;
+  return contributors[Math.floor(Math.random() * contributors.length)];
+});
+</script>
