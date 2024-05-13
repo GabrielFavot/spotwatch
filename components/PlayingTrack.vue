2c1f7bc18677b98e2f0f5609d4638966e2605390
Gabriel Favot <Gabriel.favotwurtz@gmail.com>
2024-05-13 13:23:21 +0200

    add alt text and underline on artist hover

--- components/PlayingTrack.vue
+++ components/PlayingTrack.vue
@@ -2,17 +2,17 @@
   <div v-if="playedItem" class="relative grid place-items-center min-h-screen p-5">
     <div
       class="absolute inset-0 bg-cover filter blur-[50px] scale-125"
-      :style="{ backgroundImage: `url(${playedItem.album?.images[0].url})` }"
+      :style="{ backgroundImage: `url(${playedItem.album.images[0].url})` }"
     />
     <div class="relative bg-spotify-dark shadow-lg rounded p-3 md:max-w-80">
-      <img class="w-full md:w-74 block rounded" :src="playedItem.album?.images[0].url" alt="">
+      <img class="w-full md:w-74 block rounded" :src="playedItem.album.images[0].url" :alt="playedItem.album.name">
       <div class="p-5">
         <h3 class="text-white text-lgt text-clip overflow-hidden">
           {{ playedItem.name }} {{ playedItem.explicit ? '🅴' : '' }}
         </h3>
         <span :key="i" v-for:="(artist, i) in playedItem.artists">
           <a
-            class=" text-spotify-lightgray inline"
+            class=" text-spotify-lightgray inline hover:underline"
             :href="artist.uri"
             target="_blank"
           >{{ artist.name }}</a>
