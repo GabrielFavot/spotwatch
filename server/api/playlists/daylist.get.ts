f65505d55386c824f0322318f5f7ba3744e8995c
Gabriel Favot <Gabriel.favotwurtz@gmail.com>
2024-10-06 23:43:23 +0200

    feat: hardcode daylist

--- server/api/playlists/daylist.get.ts
+++ server/api/playlists/daylist.get.ts
@@ -1,9 +1,11 @@
 export default defineEventHandler(async () => {
   const spotify = await useSpotify();
 
-  const playlists = await spotify.currentUser.playlists.playlists();
+  // const playlists = await spotify.currentUser.playlists.playlists();
 
-  const daylistId = playlists.items.find(playlist => playlist.name.startsWith('daylist'))?.id;
+  // const daylistId = playlists.items.find(playlist => playlist.name.startsWith('daylist'))?.id;
+
+  const daylistId = '37i9dQZF1EP6YuccBxUcC1';
 
   if (!daylistId) {
     return undefined;
