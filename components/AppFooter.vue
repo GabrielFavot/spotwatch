617df3841d964f0092ba83a23a80448d04e16aeb
Gabriel Favot <Gabriel.favotwurtz@gmail.com>
2024-10-06 23:57:31 +0200

    feat: remove footer contribution

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -3,13 +3,16 @@
     <a href="https://github.com/GabrielFavot/spotwatch" target="_blank">
       <Icon class="text-spotify-lightgray hover:text-spotify-green" name="octicon:mark-github-16" />
     </a>
-    <FooterContributors v-if="contributors" :contributors="contributors" />
+    <div class="flex flex-row items-center text-spotify-lightgray font-thin gap-2">
+      <Icon name="ic:baseline-code" class="text-spotify-green" />
+      <span>with</span>
+      <Icon name="material-symbols:favorite-outline" class="text-red-400 animate-pulse" />
+      <span>by</span>
+      <a :href="'https://github.com/GabrielFavot'" target="_blank" class="hover:underline font-normal">Gab</a>
+    </div>
     <div />
   </footer>
 </template>
 
 <script lang="ts" setup>
-const { data: contributors } = await useLazyFetch<{ login: string, html_url: string }[]>('https://api.github.com/repos/GabrielFavot/spotwatch/contributors', {
-  key: 'contributors',
-});
 </script>
