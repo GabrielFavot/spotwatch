26788fc2b7fe5e7f7b2ae166aaf71888eebb5e49
Gabriel Favot <Gabriel.favotwurtz@gmail.com>
2024-10-07 14:17:17 +0200

    feat: platlist footer as a link

--- components/FooterPlaylist.vue
+++ components/FooterPlaylist.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="flex justify-center">
-    <p class="text-xs text-gray-400 opacity-50">
-      #{{ playlistId }} | {{ snapshotId }}
-    </p>
+    <NuxtLink
+      :to="`/playlist/${playlist.id}`"
+      class="text-xs text-gray-400 opacity-50 hover:text-white hover:opacity-100 transition-colors"
+    >
+      #{{ playlist.id }} | {{ playlist.snapshot_id }}
+    </NuxtLink>
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { SimplifiedPlaylist } from '@spotify/web-api-ts-sdk';
+
 defineProps<{
-  playlistId: string;
-  snapshotId: string;
+  playlist: SimplifiedPlaylist
 }>();
 </script>
