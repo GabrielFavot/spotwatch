ef60ce088d086fcbcd8831d44192d35dfa22b647
Gabriel Favot <Gabriel.favotwurtz@gmail.com>
2024-11-10 11:21:08 +0100

    fix: limite size of history

--- server/api/me/history.get.ts
+++ server/api/me/history.get.ts
@@ -1,10 +1,16 @@
 import type { RecentlyPlayedTracksPage } from '@spotify/web-api-ts-sdk';
 import { HISTORY_TRACK_STORAGE_KEY } from '~/server/tasks/history/refresh';
 
+const MAX_HISTORY_ITEMS = 200;
+
 export default defineEventHandler(async (_event) => {
   const storage = useStorage('spotify');
 
   runTask('history:refresh');
 
-  return storage.getItem<RecentlyPlayedTracksPage['items']>(HISTORY_TRACK_STORAGE_KEY);
+  const items = await storage.getItem<RecentlyPlayedTracksPage['items']>(HISTORY_TRACK_STORAGE_KEY, {
+    defaultValue: [],
+  });
+
+  return items?.slice(0, MAX_HISTORY_ITEMS) ?? [];
 });
